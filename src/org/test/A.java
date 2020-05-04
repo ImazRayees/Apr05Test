b5074ed373c8173c9b2b229dda6342a4b3cc071f
ImazRayees <imazrayees@gmail.com>
2020-05-04 19:30:47 +0530

    imaz added new file

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -2,7 +2,8 @@ package org.test;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("hi rayees");
+		System.out.println("hello imaz");
 	}
 
 }
